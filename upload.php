e2de0da91f2c8f6a7f3bf3c1af679b4a2a7ef38e
radita0 <50124843+radita0@users.noreply.github.com>
2019-05-08 16:51:21 +0700

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,11 @@
+<html>
+<title>Form Upload</title>
+<body>
+<form enctype="multipart/form-data" method="POST" action="hasil_upload.php">
+File yang di upload : <input type="file" name="fupload"><br>
+Deskripsi File : <br>
+<textarea name="deskripsi" rows="8" cols="40"></textarea><br>
+<input type=submit value=Upload>
+</form>
+
+//RADITA AGUS P. 17.01.55.5005
